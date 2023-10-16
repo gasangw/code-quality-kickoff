beed7748235df632a1860646790b70cdbadc345b
gasangw <thomasgasangwa@gmail.com>
2023-10-16 12:13:25 +0200

    Add comments to the passwordPenalty

--- src/improvement-2.js
+++ src/improvement-2.js
@@ -6,10 +6,15 @@
 
 // FUNCTION 2
 const onlyGetFoods = (items)=> {
+  // create an array to hold all foods
   const ARRAY_OF_FOODS = [];
+  // loop through an array of items to access each property of 
   for(let food of items){
+    // check the type property on each object if its equal to Food and if so push to the new array that we created
+    // else if not just return the original array.
     food.type === "Food" ? ARRAY_OF_FOODS.push(food) : ARRAY_OF_FOODS
   }
+  // return the array we created.
   return ARRAY_OF_FOODS;
 }
 
